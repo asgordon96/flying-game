832b7b1f09c93ebf50289c67b6893d2d66bd055f
Aaron Gordon <asgordon96@gmail.com>
2015-01-08 18:01:35 -0800

    Remove files that were no longer used

--- js/game.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "game");
-
-game.state.add("Preloader", Preloader);
-game.state.add("Menu", Menu);
-game.state.add("Game", MainGame);
-
-game.state.start("Preloader");
